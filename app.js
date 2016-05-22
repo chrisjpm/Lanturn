f74a2771cf1a4e49b5d608e6e6957fdc1ecb64f8
cj1m <cj1mckay@gmail.com>
2016-05-22 21:48:27 +0100

    Removed favicon in app.js

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ var routes = require('./routes');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon(path.join(__dirname, 'public/images/icons', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
