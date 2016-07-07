64501808cfea19992ac9b635c59d472fab4c087d
Chris <chrispercevalmaxwell@gmail.com>
2016-07-07 23:11:24 +0100

    sidebar stuffs

--- public/javascripts/sidebar-init/init.js
+++ public/javascripts/sidebar-init/init.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  $(".leftSB").sidebar();
+  $(".left-sb").sidebar();
   $(".toggleLeftSb").on('click', function() {
-    $(".leftSB").trigger("sidebar:toggle");
+    $(".left-sb").trigger("sidebar:toggle");
+	});
+
+  $(".right-sb").sidebar({side: "right"});
+  $(".toggleRightSb").on('click', function() {
+    $(".right-sb").trigger("sidebar:toggle");
 	});
 });
