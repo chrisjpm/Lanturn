1bb3526784a62485975627c2c77a7f4be05fd17c
Christopher McKay <Christopher McKay>
2016-07-14 00:45:39 +0100

    testing

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index.hbs', {pagename: 'HomePPP'});
+  res.render('index.hbs', {pagename: 'Home'});
 });
 
 module.exports = router;
