5571fbdaeaa42d5f5c7b07d234745f0da2d029cd
Cj1m <Cj1mckay@gmail.com>
2016-08-24 17:50:18 +0100

    Proxied party images

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
+app.use('/prox', require('iproxy'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/public', express.static(__dirname + '/public'));
 app.use(favicon(__dirname + '/public/images/icons/favicon.ico'));
